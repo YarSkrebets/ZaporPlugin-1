f1021b42035a9dea51c2c9d20c2e5c397926ac2b
DankaParvan <43324251+DankaParvan@users.noreply.github.com>
2021-01-28 02:22:35 +0300

    1

--- src/main/java/ru/danka/zapor/ZaporListener.java
+++ src/main/java/ru/danka/zapor/ZaporListener.java
@@ -1,25 +1,49 @@
 package ru.danka.zapor;
 
 import net.minecraft.server.v1_12_R1.NBTTagCompound;
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.block.CreatureSpawner;
 import org.bukkit.craftbukkit.v1_12_R1.entity.CraftItem;
 import org.bukkit.craftbukkit.v1_12_R1.inventory.CraftItemStack;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Sheep;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class ZaporListener implements Listener {
     private JavaPlugin plugin;
 
     public ZaporListener(JavaPlugin plugin) {
         this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void sheepShearedSpawner(BlockPlaceEvent e){
+        Location location = e.getBlockPlaced().getLocation();
+        location.add(0,1,0);
+        Sheep sheep = location.getWorld().spawn(location, Sheep.class);
+        sheep.setSheared(true);
+        sheep.setCustomName("Пажилая овца");
+        sheep.setCustomNameVisible(true);
+    }
+    @EventHandler
+    public void sheepPinkSpawner (BlockBreakEvent e){
+        Player player = e.getPlayer();
+        Location location = e.getBlock().getLocation();
+        Sheep sheep = location.getWorld().spawn(location, Sheep.class);
+        sheep.setColor(DyeColor.PINK);
+        sheep.setAI(false);
+        sheep.setCustomName( ChatColor.LIGHT_PURPLE +"Розовая овца");
+        sheep.setCustomNameVisible(true);
+
     }
 
     @EventHandler
--- src/main/java/ru/danka/zapor/ZaporPlugin.java
+++ src/main/java/ru/danka/zapor/ZaporPlugin.java
@@ -13,6 +13,8 @@ import org.bukkit.entity.Zombie;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Random;
+
 public final class ZaporPlugin extends JavaPlugin {
 
     @Override
@@ -48,11 +50,11 @@ public final class ZaporPlugin extends JavaPlugin {
             item = CraftItemStack.asBukkitCopy(nmsItem);
 
             player.getInventory().addItem(item);
-            
+
             return true;
         });
 
-        getCommand("габрираЗапор").setExecutor((sender, command, label, args)->{
+        getCommand("габрираЗапор").setExecutor((sender, command, label, args) -> {
             Player player = (Player) sender;
             //по запористому
 //            Location location = player.getLocation();
@@ -62,7 +64,7 @@ public final class ZaporPlugin extends JavaPlugin {
 //            location.add(location.getDirection().multiply(5));
 //            location.add(0,2,0);
             Location target = player.getTargetBlock(null, 30).getLocation();
-            target.add(0,1,0);
+            target.add(0, 1, 0);
             Skeleton skeleton = target.getWorld().spawn(target, Skeleton.class);
             skeleton.getEquipment().setItemInMainHand(new ItemStack(Material.GOLD_SWORD));
             skeleton.setCustomName(ChatColor.GOLD + "ДОЛБАЕБ С ЗОЛОТЫМ МЕЧОМ");
@@ -70,7 +72,7 @@ public final class ZaporPlugin extends JavaPlugin {
             return true;
         });
 
-        getCommand("лехаблять").setExecutor((sender, command, label, args) ->{
+        getCommand("лехаблять").setExecutor((sender, command, label, args) -> {
             Player player = (Player) sender;
             player.getEquipment().setBoots(new ItemStack(Material.DIAMOND_BOOTS));
             player.getEquipment().setChestplate(new ItemStack(Material.DIAMOND_CHESTPLATE));
@@ -78,6 +80,44 @@ public final class ZaporPlugin extends JavaPlugin {
             player.getEquipment().setLeggings(new ItemStack(Material.DIAMOND_LEGGINGS));
             player.getEquipment().setItemInMainHand(new ItemStack(Material.DIAMOND_SWORD));
             //player.setFlying(true); не работает да и хуй с ним
+
+            return true;
+        });
+
+        getCommand("раздачанаспаве").setExecutor((sender, command, label, args) -> {
+            sender.sendMessage("ща навалю");
+            int chance = 0;
+            try {
+                chance = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                sender.sendMessage("ДЕБИЛ ПИШИ ЦИФРЫ");
+                return true;
+            }
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                if (player.equals(sender)){
+                    continue;
+                }
+                if ((Math.random() * 100) <= chance) {
+                    player.getInventory().addItem(new ItemStack(Material.BEACON)); // ЭТО БЕКОН
+
+                }
+            }
+            return true;
+        });
+
+        getCommand("писюн").setExecutor((sender, command, label, args) -> {
+            Player player = (Player) sender;
+            Location target = player.getTargetBlock(null, 30).getLocation();
+            target.getBlock().setType(Material.COBBLESTONE);
+            target.add(1, 0, 0);
+            target.getBlock().setType(Material.COBBLESTONE);
+            target.add(-2, 0, 0);
+            target.getBlock().setType(Material.COBBLESTONE);
+            target.add(1, 1, 0);
+            target.getBlock().setType(Material.COBBLESTONE);
+            target.add(0, 1, 0);
+            target.getBlock().setType(Material.REDSTONE_BLOCK);
+
 
             return true;
         });
